f0b4899104ebeba7baac4c879241c3ad67841457
humberto <humbertorivero9@gmail.com>
2020-04-29 23:22:38 -0300

    cambios hasta formulario

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle ('active');
 });
 
-btnAbrirFormulario.addEventListener('click', () => {
+btnAbrirFormulario.addEventListener('click', () => {  
     btnAbrirFormulario.classList.toggle('active');
 });
